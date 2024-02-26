b966ed9c646300a80ec598ac0b0c7df1b64c292f
WeyShi <chengzhi_shi@163.com>
2024-02-26 17:07:46 -0500

    NP and scripts

--- scripts/for_batch_run_baselines.bash
+++ scripts/for_batch_run_baselines.bash
@@ -3,25 +3,33 @@
 # conda activate dsk
 my_dir = /work/DNAL/shi.cheng/NPP/Satellite_Fusion
 # cd $pwd
+n = 1000
 
-for dataset in PinMNIST Sythetic Building
+for dataset in PinMNIST #Synthetic Building
 do
     for feature in AE DDPM
     do
-        for mode in mesh random
+        for partial_percent in '0.2' '0.5' '0.8'
         do
-            for n in '100' '1000'
+            for mode in mesh random
             do
-                for d in '3' '10'
-                do
-                    for n_pins in '10' '100'
+                if [ "$mode" == "mesh" ]; then
+                    for d in '3' '10'
                     do
-                        for partial_percent in '0.2' '0.5' '0.8'
+                        for n_pins in '10'
                         do
                             sbatch /my_dir/batch_run_baselines.bash $dataset $feature $mode $n $d $n_pins $partial_percent
-                        do
+                        done
+                    done
+                elif [ "$mode" == "random" ]; then
+                    for d in '10'
                     do
-                done
+                        for n_pins in '10' '100'
+                        do       
+                            sbatch /my_dir/batch_run_baselines.bash $dataset $feature $mode $n $d $n_pins $partial_percent
+                        done
+                    done
+                fi
             done
         done
     done
