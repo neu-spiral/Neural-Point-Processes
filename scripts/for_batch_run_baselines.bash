cd9e25a42bd91089ce9f1772fbb1a56c100b44e9
WeyShi <chengzhi_shi@163.com>
2024-03-29 23:40:41 -0400

    single run supported with modalities

--- scripts/for_batch_run_baselines.bash
+++ scripts/for_batch_run_baselines.bash
@@ -4,7 +4,7 @@
 
 for dataset in Building #PinMNIST Synthetic
 do
-    for feature in DDPM
+    for feature in AE
     do
          for d in 10 #32
          do
@@ -12,16 +12,39 @@ do
              do
                  for n_pins in 10
                  do
-                     for name in Mesh_new
+                     for name in Mesh_MM
                      do
-                         for sigma in 0 #0.1 0.2
-                         do
-                            echo "Executing training: dataset $dataset, feature $feature, mode mesh, d: $d"
-                            sbatch ./scripts/batch_run_baselines.bash $dataset $feature $mode $d $n_pins $name $sigma
-                         done
+                        for modality in PS-RGBNIR-SAR PS-RGB #PS-RGBNIR  
+                        do
+                            for sigma in 0 0.1
+                            do
+                                echo "Executing training: dataset $dataset, feature $feature, mode mesh, d: $d"
+                                sbatch ./scripts/batch_run_baselines.bash $dataset $feature $mode $d $n_pins $name $sigma $modality
+                            done
+                        done
                      done
                  done
              done
          done
+        for n_pins in 10 #100
+        do
+            for mode in random
+            do
+                for d in 10
+                do
+                    for name in Random_MM
+                    do
+                        for modality in PS-RGBNIR-SAR PS-RGB #PS-RGBNIR 
+                        do
+                            for sigma in 0 0.1
+                            do
+                                echo "Executing training: dataset $dataset, feature $feature, mode random, npins $n_pins"
+                                sbatch ./scripts/batch_run_baselines.bash $dataset $feature $mode $d $n_pins $name $sigma $modality
+                            done
+                        done
+                    done
+                done
+            done
+        done
     done
 done
